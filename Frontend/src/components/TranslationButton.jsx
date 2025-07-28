61b8eee2ae260017bf02841d1418f7376bafd82c
RohitDhule <dhulerohit970@gmail.com>
2025-07-28 14:49:28 +0530

    Create TranslationButton.jsx

---
+++ Frontend/src/components/TranslationButton.jsx
@@ -0,0 +1,11 @@
+import {Languages} from "lucide-react";
+
+const TranslationButton = () => {
+  return (
+    <button className="btn btn-success btn-sm text-white">
+      <Languages className="size-6" />
+    </button>
+  )
+}
+
+export default TranslationButton;
