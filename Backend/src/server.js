0e350d17518b0eaa7e36f91ad271fd8bac4ceb68
Rohit Dhule <dhulerohit970@gmail.com>
2025-08-22 09:59:03 +0530

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -32,10 +32,10 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/translation", translationRoutes); // Use the new route
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
 
